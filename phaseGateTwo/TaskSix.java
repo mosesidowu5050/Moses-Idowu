0efb23ca3da2198637362b2e7d8d786b9f764244
idowu moses <dynamopraize5050@gmail.com>
2025-01-16 15:25:27 +0100

    first commit

---
+++ phaseGateTwo/TaskSix.java
@@ -0,0 +1,22 @@
+public class TaskSix{
+
+public Boolean unsortedArrays(int[][] arraysOne){
+
+int count = 0;
+
+for (int c = 0; c < arraysOne.length; c++){
+for (int b = 0; b < arraysOne[c]; b++){
+	
+    }
+  }
+return Arrays.sort(arraysOne);
+}
+
+public static void main(String[] args){
+
+int[][] arraysOne = {{3, 1, 5}, {8, 6, 4}}; 
+
+System.out.print(unsortedArrays(arraysOne));
+
+  }
+}
